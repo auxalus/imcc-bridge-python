28a55cfcab5a658f473db0c1eb392e2c2a002c64
auxalus <142904510+auxalus@users.noreply.github.com>
2026-09-10 15:19:33 +0530

    new commit 1

--- demo.py
+++ demo.py
@@ -2,6 +2,10 @@
 # print("Number is odd :", (bool(a%2)))
 
 
-age_in_years = int(input("add age in years:"))
+# age_in_years = int(input("add age in years:"))
 
-print (age_in_years,"years = " ,(age_in_years*365))
+# print (age_in_years,"years = " ,(age_in_years*365))
+
+time_in_minutes = int(input("add time in minutes: "))
+
+print(time_in_minutes, "is", (time_in_minutes//60), "hours", (time_in_minutes%60), "minutes" )
