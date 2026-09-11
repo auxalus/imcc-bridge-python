b8cb1bfea9439fb8b237ddc61170264d498c22f2
auxalus <142904510+auxalus@users.noreply.github.com>
2026-09-11 15:36:35 +0530

    needs to be checked

---
+++ dateanalogy.py
@@ -0,0 +1,18 @@
+userinput = int(input("add date"))
+
+if userinput==1:
+  print("monday")
+elif userinput==2:
+  print("tue")
+elif userinput==3:
+  print("wed")
+elif userinput==4:
+  print("thu")
+elif userinput==5:
+  print("fri")
+elif userinput==6:
+  print("sat")
+elif userinput==7:
+  print("sun")
+else:
+  print("invalid number enterred")
